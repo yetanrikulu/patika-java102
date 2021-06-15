0b5db7e33a3616bcc7d910b114b5de01e97c80c2
Yaemta <yaemta@gmail.com>
2021-06-15 23:29:08 +0300

    first commit

--- src/maceraOyunu/Game.java
+++ src/maceraOyunu/Game.java
@@ -1,4 +1,18 @@
 package maceraOyunu;
 
+import java.util.Scanner;
+
 public class Game {
+
+    private Scanner input = new Scanner (System.in);
+
+
+    public void start(){
+        System.out.println("Macera Oyununa Hoşgeldiniz !");
+        System.out.println("Lütfen bir isim giriniz : ");
+        String playerName = input.nextLine();
+
+        Player player = new Player(playerName);
+
+    }
 }
---
+++ src/maceraOyunu/Player.java
@@ -0,0 +1,54 @@
+package maceraOyunu;
+
+public class Player {
+
+    private int damage;
+    private int health;
+    private int money;
+    private String charName;
+    private String name;
+
+    public Player (String name){
+        this.name = name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public String getCharName() {
+        return charName;
+    }
+
+    public void setCharName(String charName) {
+        this.charName = charName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
